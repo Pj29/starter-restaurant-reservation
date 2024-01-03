1e3bf8e9735fbfef91f912de212828ded61e5035
Peter Andrew Jones <pj@peters-mbp.mynetworksettings.com>
2024-01-02 22:38:21 -0500

    US-01 finish reservation form

--- front-end/src/reservations/NewReservation.js
+++ front-end/src/reservations/NewReservation.js
@@ -6,37 +6,58 @@ import ReservationForm from "./ReservationForm";
 import { previous } from "../utils/date-time";
 
 function NewReservation() {
-    const history = useHistory();
-    const [error, setError] = useState(null);
+  const history = useHistory();
+  const [error, setError] = useState(null);
 
-    const initialReservationForm = {
-        first_name: "",
-        last_name: "",
-        mobile_number: "",
-        reservation_date: "",
-        reservation_time: "",
-        people: "",
-    };
+  const initialReservationForm = {
+    first_name: "",
+    last_name: "",
+    mobile_number: "",
+    reservation_date: "",
+    reservation_time: "",
+    people: "",
+  };
 
-    const [reservation, setReservation] = useState(initialReservationForm);
+  const [reservation, setReservation] = useState(initialReservationForm);
 
-    const changeHandler = useCallback(( { target }) => {
-        setReservation(previousReservation => ({ ...previousReservation, [target.name]: target.value}));
-    }, []);
+  const changeHandler = useCallback(({ target }) => {
+    setReservation((previousReservation) => ({
+      ...previousReservation,
+      [target.name]: target.value,
+    }));
+  }, []);
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-        const abortController = new AbortController();
-        try {
-            const newRes = await createReservation(reservation, abortController.signal);
-            history.push(`/dashboard?date=${newReservation.reservation_date.slice(0, 10)}`);
-        } catch (error) {
-            setError(error);
-        }
-
-        return () => abortController.abort();
+    const abortController = new AbortController();
+    try {
+      const newRes = await createReservation(
+        reservation,
+        abortController.signal
+      );
+      history.push(
+        `/dashboard?date=${newReservation.reservation_date.slice(0, 10)}`
+      );
+    } catch (error) {
+      setError(error);
     }
 
-    return ()
+    return () => abortController.abort();
+  };
+
+  return (
+    <main>
+      <div>
+        <h1>Create a New Reservation</h1>
+        <ErrorAlert error={error} setError={setError} />
+      </div>
+
+      <ReservationForm
+        reservation={reservation}
+        handleSubmit={handleSubmit}
+        changeHandler={changeHandler}
+      />
+    </main>
+  );
 }
